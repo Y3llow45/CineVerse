bcbad92fa97783603ba97817f95d1f1baccbdbbc
Y3llow45 <hristovborislav45@gmail.com>
2025-12-12 18:44:46 +0200

    Create UserFile.java

---
+++ src/main/java/com/example/CineVerse/entity/UserFile.java
@@ -0,0 +1,34 @@
+package com.example.CineVerse.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user_files")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserFile {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(nullable = false)
+    private String fileName;
+
+    @Column(nullable = false)
+    private String contentType;
+
+    @Lob
+    @Column(nullable = false)
+    private byte[] data;
+
+    private long size;
+}
